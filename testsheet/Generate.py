6debd1691f9f8caa54faa9d49040f53455da5d70
Padraig Gleeson <p.gleeson@gmail.com>
2021-12-13 11:09:19 +0000

    Python & others

--- testsheet/Generate.py
+++ testsheet/Generate.py
@@ -8,21 +8,29 @@ info = """
 
 count = 0
 
-info += '  | Model | Tests | PRs |\n'
-info += '  |----------|:------:|:------:|\n'
+
+allrefs = {'Core Java repositories': jrefs,
+           'Core Python repositories': pyrefs,
+           'Related repositories': otherrefs,}
+
+for cat in allrefs:
+    info += '\n### %s\n\n'%cat
+
+    info += '| Model | Tests | PRs |\n'
+    info += '|----------|:------:|:------:|\n'
+
+    refs = allrefs[cat]
+    for name in refs.keys():
+        ref = refs[name]
+
+        print("Looking at: %s in %s"%(ref, name))
+
+        info += '| <a href="https://github.com/%s">%s</a> |'%(ref,name)
+        info += '  [![OMV](https://github.com/%s/actions/workflows/ci.yml/badge.svg)](https://github.com/%s/actions/workflows/ci.yml) | '%(ref,ref)
+        info += '  [![GitHub pull requests](https://img.shields.io/github/issues-pr/%s)](https://github.com/%s/pulls) | \n'%(ref,ref)
 
 
-for name in jrefs.keys():
-    ref = jrefs[name]
-
-    print("Looking at: %s in %s"%(ref, name))
-
-    info += ' | <a href="https://github.com/%s">%s</a> |'%(ref,name)
-    info += '  [![OMV](https://github.com/%s/actions/workflows/ci.yml/badge.svg)](https://github.com/%s/actions/workflows/ci.yml) | '%(ref,ref)
-    info += '  [![GitHub pull requests](https://img.shields.io/github/issues-pr/%s)](https://github.com/%s/pulls) | \n'%(ref,ref)
-
-
-    count+=1
+        count+=1
 
 info += '  </table>\n'
 
--- testsheet/RepoInfo.py
+++ testsheet/RepoInfo.py
@@ -1,6 +1,8 @@
 import collections
 
 jrefs = collections.OrderedDict()
+pyrefs = collections.OrderedDict()
+otherrefs = collections.OrderedDict()
 
 
 jrefs['org.neuroml.model.injectingplugin'] = 'NeuroML/org.neuroml.model.injectingplugin'
@@ -9,3 +11,15 @@ jrefs['org.neuroml.model'] = 'NeuroML/org.neuroml.model'
 jrefs['org.neuroml.export'] = 'NeuroML/org.neuroml.export'
 jrefs['org.neuroml.import'] = 'NeuroML/org.neuroml.import'
 jrefs['jNeuroML'] = 'NeuroML/jNeuroML'
+
+pyrefs['LEMS'] = 'LEMS/LEMS'
+pyrefs['pylems'] = 'LEMS/pylems'
+pyrefs['libNeuroML'] = 'neuralensemble/libNeuroML'
+pyrefs['pyNeuroML'] = 'NeuroML/pyNeuroML'
+pyrefs['NeuroMLlite'] = 'NeuroML/NeuroMLlite'
+
+
+otherrefs['OMV'] = 'OpenSourceBrain/osb-model-validation'
+otherrefs['OpenCortex'] = 'OpenSourceBrain/OpenCortex'
+otherrefs['pyelectro'] = 'neuralensemble/pyelectro'
+otherrefs['neurotune'] = 'neuralensemble/neurotune'
